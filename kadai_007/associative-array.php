fe8e44c3d832474759cfaf72b69ed5ce6d9d130d
tpmx89 <tpmx.d.728qyu@icloud.com>
2024-07-11 08:25:18 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>kadai_007</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+         $merchandise_data = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
+         print_r($merchandise_data);
+         ?>
+     </p>
+ </body>
+ 
+ </html>
